b3cf495f1199232e10316ae4f87a3880f5b6c7d4
deluxesande <deluxesande@gmail.com>
2025-06-28 02:28:37 +0300

    Added dark icons for light status bar

--- app/src/main/java/com/example/tourist_app_android/MainActivity.java
+++ app/src/main/java/com/example/tourist_app_android/MainActivity.java
@@ -15,6 +15,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.tourist_app_android.fragments.EventsPage;
@@ -43,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // For dark icons on a light background (default for white status bar)
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.bottom_home);
